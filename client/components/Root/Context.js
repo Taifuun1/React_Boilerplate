21172eedc3c5b16858b369937282e4c746504314
Henri Tulus <h3tulus@hotmail.com>
2021-02-20 19:51:09 +0200

    Create React boilerplate with Webpack and Babel

---
+++ client/components/Root/Context.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Context = () => {
+
+	return (
+		<div>
+			<span>
+				Tekstiä
+			</span>
+		</div>
+	)
+};
+
+export default Context;
---
+++ client/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { render } from "react-dom";
+
+import Context from "./components/Root/Context";
+
+import "./styles/base.scss";
+
+render(<Context />, document.getElementById("root"));
